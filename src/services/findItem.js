de54f86ba2be5826b9ef1a2304f2737757a0457b
nadim <mdnadimmahamud704@gmail.com>
2023-08-08 13:52:36 +0600

    start new lesson from today

--- src/services/findItem.js
+++ src/services/findItem.js
@@ -1,4 +1,4 @@
-const createHttpError = require('http-errors');
+const createError = require('http-errors');
 
 const mongoose = require('mongoose');
 
@@ -6,10 +6,7 @@ const findWithId = async (Model, id, options = {}) => {
   try {
     const item = await Model.findById(id, options);
     if (!item) {
-      throw createHttpError(
-        404,
-        `${Model.modelName} does not exist with this id `
-      );
+      throw createError(404, `${Model.modelName} does not exist with this id `);
     }
     return item;
   } catch (error) {
